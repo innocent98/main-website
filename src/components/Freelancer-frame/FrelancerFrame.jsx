f80f26184342439456bc97dd8adf4c156ecb3a20
michaelAladejuyigbe <michaelaladejuyigbe@gmail.com>
2024-03-25 06:16:47 +0100

    Your commit message

--- src/components/Freelancer-frame/FrelancerFrame.jsx
+++ src/components/Freelancer-frame/FrelancerFrame.jsx
@@ -4,7 +4,8 @@ import image1 from "../../assets/Asset 4figma 1.png"
 
 const FrelancerFrame = () => {
   return (
-    <div className='frelancer-frame'>
+    <>
+    <div className='desktop frelancer-frame'>
 
         <div className='section-image'>
           <img src={image1} alt='client'/>
@@ -22,6 +23,28 @@ const FrelancerFrame = () => {
         
 
     </div>
+
+    <div className='mobile frelancer-frame'>
+
+    <div className='section-title'>
+        <div>For freelancers</div>
+        <h2> Browse for your job choice </h2>
+        <p>Connect with inspiring clients who fuel your enthusiasm and elevate your career or business to unprecedented levels.</p>
+        <div className='btn'>Find Jobs</div>
+        </div>
+
+        <div className='section-image'>
+          <img src={image1} alt='client'/>
+        </div>
+     
+     
+        
+      
+        
+        
+
+    </div>
+    </>
   )
 }
 
