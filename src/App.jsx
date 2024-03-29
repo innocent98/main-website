14e8bcc6b4a76c93046e90c9a0eaca3d4b98b164
Victor Oluwatosin <tosinadebayo55@gmail.com>
2024-03-29 00:19:03 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import About from './pages/aboutpage/About'
-import LandingPage from './pages/landingpage/LandingPage'
-import Layout from './Layout'
-import FaqPage from './components/faq-frame/FaqPage'
-import AfilliatePage from './pages/affiliate-page/AfilliatePage'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./pages/aboutpage/About";
+import LandingPage from "./pages/landingpage/LandingPage";
+import Layout from "./Layout";
+import FaqPage from "./components/faq-frame/FaqPage";
+import AfilliatePage from "./pages/affiliate-page/AfilliatePage";
 
 function App() {
-
   return (
-     <div>
-        <Routes>
-          <Route path="/" element={<Layout />}>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<LandingPage />} />
           <Route path="/about" element={<About />} />
           <Route path="/faq" element={<FaqPage />} />
           <Route path="/affiliate" element={<AfilliatePage />} />
         </Route>
       </Routes>
-     </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
